e8bd4d423665bdd6c01d314799d3b16c37c6b2c0
Piyumi <piyumikumari1995@gmail.com>
2021-05-14 18:32:47 +0530

    Expense Recorder

---
+++ routes/transactions.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//const { getTransactions } = require('../client/controllers/transactions');
+
+//router.get('/',(req, res)=> res.send('Hello'));
+
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
+
+
+router
+  .route('/')
+  .get(getTransactions)
+  .post(addTransaction);
+
+router
+  .route('/:id')
+  .delete(deleteTransaction);
+
+module.exports = router;
